1a4a28edec6e055ac899dbb178c17f68eaa0e202
Tu Nguyen <nvminhtu@gmail.com>
2019-08-11 17:25:10 +0700

    vue JS demo

---
+++ 03.vuejs/index.js
@@ -0,0 +1,18 @@
+var app4 = new Vue({
+    el : '#app-4',
+    data : {
+        todos: [
+            { text : 'Vietnam' },
+            { text : 'Malaysia' },
+            { text : 'Singapore' },
+            { text : 'Thailand' }
+        ]
+    }
+});
+
+var app1 = new Vue({
+    el: '#app-1',
+    data : {
+        seen : true
+    }
+});
