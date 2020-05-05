7508f0c2f6c64afbbbbce851d0097b285c7c9764
Tu Nguyen <nvminhtu@gmail.com>
2020-05-05 15:12:38 +0700

    vuejs data

--- 07.vuejs/index.js
+++ 07.vuejs/index.js
@@ -1,46 +1,59 @@
-var app4 = new Vue({
-    el : '#app-4',
-    data : {
-        todos: [
-            { text : 'Vietnam' },
-            { text : 'Malaysia' },
-            { text : 'Singapore' },
-            { text : 'Thailand' }
-        ]
-    }
-});
-
 var app1 = new Vue({
     el: '#app-1',
-    data : {
+    data: {
         seen : true
     }
 });
 
 var app2 = new Vue({
     el: '#app-2',
-    data : {
-        parentMessage : 'Parent',
-        items: [
-            { message : 'One Piece' },
-            { message : 'Dragon Fighter Z' }
+    data: {
+        brand : 'Honda',
+        items : [
+            { model : 'Vision' },
+            { model : 'Airblade' },
+            { model : 'SH Mode' }
         ]
     }
 })
 
+var app4 = new Vue({
+    el : '#app-4',
+    data : {
+        countries: [
+            { name : 'Vietnam' },
+            { name : 'Malaysia' },
+            { name : 'Singapore' },
+            { name : 'Thailand' }
+        ]
+    }
+});
+
+
 var app5 = new Vue({
     el: '#app-5',
     data : {
-        rawHtml : '<span style="color:red">Amazing Cool</span>'
+        message: 'Hello, welcome to Vue.js!'
+    },
+    methods: {
+        daoMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 })
 
-
 var app6 = new Vue({
     el: '#app-6',
-    data: {
-        name : 'Torress',
-        age : 19,
-        from : 'Dong Nai'
+    data : {
+        message: 'Hello Vue here!!!'
     }
+})
+
+// * Write Compontent **//
+Vue.component('todo-item', {
+    template: '<li>This is a todo</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7'
 })
